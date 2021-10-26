82586aa52ad50993e1606920a196e5e67cccebac
chandelierephrata <chandephrata@gmail.com>
2021-10-25 18:32:38 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+
+const NewCats=[...cats,"Felix"]
+// cats.push("Tom")
+// cats.unshift("Rose")
+// cats.pop()
+// cats.shift
+//cats.push("Felix")
+
+function  destructivelyAppendCat(name){
+    console.log(name)
+cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    console.log(name)
+cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    console.log()
+cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    console.log()
+cats.shift()
+}
+function appendCat(name){
+    console.log(name)
+cats.push(name)
+}
+

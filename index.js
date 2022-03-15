0bf2ddea0a57d59834ef86ac77a37c5dfef86699
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-15 11:01:34 -0600

    commit tree 5a70a998947c8949034893862badd974fe30b0df

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ cats.shift()
 }
 function appendCat(name){
     console.log(name)
-cats.push(name)
+    return[...cats,name]
 }
-
+function prependCat(name){
+    return[name,...cats]
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
